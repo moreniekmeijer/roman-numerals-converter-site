513e4f386e47c52c5b242682b98f6950eaa47118
moreniekmeijer <niekjmeijer@gmail.com>
2024-12-22 13:50:01 +0100

    Slight design changes and improved int-to-roman.js

--- scripts/int-to-roman.js
+++ scripts/int-to-roman.js
@@ -4,17 +4,21 @@ function intToRoman(decimalNumber) {
         ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
         ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
         ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
-        ["", "M", "MM", "MMM"]
     ];
 
-    let romanNumber = "";
+    let cents = "";
+    let millenia = "";
     let digits = String(decimalNumber).split("").reverse();
 
-    for (let i = 0; i < digits.length; i++) {
-        romanNumber = romanMappings[i][parseInt(digits[i])] + romanNumber;
+    if (decimalNumber => 1000) {
+        millenia = "M".repeat(Math.floor(decimalNumber / 1000));
     }
-    return romanNumber;
+    for (let i = 0; i < digits.length && i < 3; i++) {
+        cents = romanMappings[i][parseInt(digits[i])] + cents;
+    }
+    return millenia + cents;
 }
+
 function convertDecimalNumber() {
     const decimalInput = document.getElementById("decimalInput").value;
     document.getElementById("romanResult").value = intToRoman(decimalInput);
--- scripts/roman-to-int.js
+++ scripts/roman-to-int.js
@@ -1,5 +1,6 @@
+let error;
+
 function romanToInt(romanNumber) {
-    // Object om de Romeinse karakters te koppelen aan hun numerieke waarde:
     const romanValues = {
         'I': 1,
         'V': 5,
@@ -10,24 +11,22 @@ function romanToInt(romanNumber) {
         'M': 1000
     };
 
-    // Initialisatie van de totale waarde en de 'vorige' waarde
+    if (romanNumber !== romanValues) {
+        error = "Not a Roman numeral.";
+    }
+
     let totalValue = 0;
     let previousValue = 0;
 
-    // Omgekeerde for-loop om de karakters te verwerken van rechts naar links
     for (let i = romanNumber.length - 1; i >= 0; i--) {
-        // Haal het huidige karakter op en converteer het naar een waarde
         const currentChar = romanNumber.toUpperCase()[i];
         const currentValue = romanValues[currentChar];
 
-        // Voeg toe of trek af op basis van de waarde
         if (currentValue >= previousValue) {
             totalValue += currentValue;
         } else {
             totalValue -= currentValue;
         }
-
-        // Update de vorige waarde
         previousValue = currentValue;
     }
     return totalValue;
@@ -35,4 +34,5 @@ function romanToInt(romanNumber) {
 function convertRomanNumber() {
     const romanInput = document.getElementById("romanInput").value;
     document.getElementById("intResult").value = romanToInt(romanInput);
+    document.getElementById("error").value = error;
 }
